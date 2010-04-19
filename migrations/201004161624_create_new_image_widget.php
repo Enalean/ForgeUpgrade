366a6ff73f9cbb5d26ff794fd443e31cbdfdd578
nouha terzi <terzino@tunlx007.tun.st.com>
2010-04-19 13:14:49 +0200

    Add scripts for codendi upgrade to 4.2

---
+++ migrations/201004161624_create_new_image_widget.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once dirname(__FILE__).'/../src/ForgeUpgradeBucket.php';
+
+/**
+ *
+ */
+class CreateNewImageWidget extends ForgeUpgradeBucket {
+
+    
+    public function description() {
+        return <<<EOT
+Creating table for the new widget image.
+
+EOT;
+    }
+
+    public function preUp() {
+        
+    }
+
+    public function up() {
+        $sql = 'CREATE TABLE IF NOT EXISTS widget_image ('.
+                ' id int(11) unsigned NOT NULL auto_increment PRIMARY KEY,'.
+                ' owner_id int(11) unsigned NOT NULL default 0,'.
+                ' owner_type varchar(1) NOT NULL default "u",'.
+                ' title varchar(255) NOT NULL,'.
+                ' url TEXT NOT NULL,'.
+                ' KEY (owner_id, owner_type)'.
+                ')';
+
+        $this->db->createTable('widget_image', $sql);
+        
+    }
+    
+    public function postUp() {
+        if (!$this->db->tableExists('widget_image')) {
+            throw new ForgeUpgradeBucketUpgradeNotCompleteException('widget_image not existing');
+        }
+        
+    }
+
+}
+
+?>
